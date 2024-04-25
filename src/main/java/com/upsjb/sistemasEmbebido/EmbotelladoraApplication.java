ba1e05111eba178fc1f54e4e2c7b44a44971366b
UPSJB <pacolucasmargarita@gmail.com>
2024-04-25 10:52:16 -0500

    Quitar jpa y postgres,unicamente es una aplicacion Statelees

--- src/main/java/com/upsjb/sistemasEmbebido/EmbotelladoraApplication.java
+++ src/main/java/com/upsjb/sistemasEmbebido/EmbotelladoraApplication.java
@@ -2,11 +2,9 @@ package com.upsjb.sistemasEmbebido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class EmbotelladoraApplication {
 
 	public static void main(String[] args) {
